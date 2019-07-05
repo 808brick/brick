12ca9aad434efb40d8a8f81f839d0d09129f5d2d
Raymond Andrade <raymond808state1@gmail.com>
2019-07-04 23:36:06 -1000

    Added SSH Plugin

---
+++ public/Plugins/ssh_ros/ssh_classes.js
@@ -0,0 +1,25 @@
+class Robot_Div {
+	constructor(document, name){
+
+		this.name = name;
+
+	}
+
+	create(){
+		return div
+	}
+
+}
+
+class Robot_Info{
+	constructor(document, name, ip, password){
+		return {"name":robot_name_input.value, "ip": computer_ip_input.value, "password":computer_password_input.value}
+	}
+
+}
+
+class Terminal_Div{
+	constructor(){
+		
+	}
+}
